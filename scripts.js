e1b68c0b76c9cbc2486f7498338905b5ecc67b68
amoolya hejjaji <amoolya.p@s.amity.edu>
2025-08-13 19:57:40 +0530

    added book no2 title,author,cost,stock

--- scripts.js
+++ scripts.js
@@ -1,4 +1,3 @@
-console.log("books caalog loaded.");
 const books = [
   {
     id: 1,
@@ -6,6 +5,13 @@ const books = [
     author: "Paulo Coelho",
     price: 250,
     stock: 10
+  },
+  {
+    id: 2,
+    title: "Rich Dad Poor Dad",
+    author: "Robert Kiyosaki",
+    price: 300,
+    stock: 5
   },
   
 ];
