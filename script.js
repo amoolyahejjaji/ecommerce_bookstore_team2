70493298bba956162542ab045b9486c3aecef82b
amoolya hejjaji <amoolya.p@s.amity.edu>
2025-08-13 21:33:05 +0530

    added some more links to displayed books using js

--- script.js
+++ script.js
@@ -26,3 +26,17 @@ books.forEach(book => {
   `;
   container.appendChild(div);
 });
+const container = document.querySelector(".book-container");
+
+books.forEach(book => {
+  const div = document.createElement("div");
+  div.className = "book";
+  div.innerHTML = `
+    <h3>${book.title}</h3>
+    <p><strong>Author:</strong> ${book.author}</p>
+    <p><strong>Price:</strong> ₹${book.price}</p>
+    <p><strong>Stock:</strong> ${book.stock}</p>
+    <button>Add to Cart</button>
+  `;
+  container.appendChild(div);
+});
