bf55b9e0af7cbf047605b222acef136e9ae37a36
amoolya hejjaji <amoolya.p@s.amity.edu>
2025-08-13 20:15:52 +0530

    added book no13  title,author,cost,stock

--- scripts.js
+++ scripts.js
@@ -96,6 +96,12 @@ const books = [
     author: "prajakatha koli",
     price: 250,
         },
+ {
+    id: 9,
+    title: "too good to be too true",
+    author: "prajakatha koli",
+    price: 250,
+        },
 
 
 
