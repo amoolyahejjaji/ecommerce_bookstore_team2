133bf81396f6e774a790bad53b033ada4cb91e0e
amoolya hejjaji <amoolya.p@s.amity.edu>
2025-08-14 20:03:32 +0530

    added add to cart alert 2

--- scripts.js
+++ scripts.js
@@ -2,3 +2,6 @@ console.log("Books catalog loaded.");
 div.querySelector("button").addEventListener("click", () => {
   alert(`"${book.title}" has been added to the cart!`);
 });
+div.querySelector("button").addEventListener("click", () => {
+  alert(`"${book.title}" has been added to the cart!`);
+});
