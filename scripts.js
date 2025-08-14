c9d7f2f548ea0eca42f391f08d4d3b79fff0023e
amoolya hejjaji <amoolya.p@s.amity.edu>
2025-08-14 19:44:24 +0530

    added low stock warning for books in pink

--- scripts.js
+++ scripts.js
@@ -2,3 +2,6 @@ console.log("books caalog loaded.");
 if (book.stock < 5) {
   div.innerHTML += `<p style="color:red;">Only few left!</p>`;
 }
+if (book.stock < 5) {
+  div.innerHTML += `<p style="color:pink;">Only few left!</p>`;
+}
