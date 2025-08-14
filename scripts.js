aeb330e03399b3769be762ee7418e0eac4c6ef27
amoolya hejjaji <amoolya.p@s.amity.edu>
2025-08-14 19:56:45 +0530

    added add to cart alert

--- scripts.js
+++ scripts.js
@@ -11,3 +11,10 @@ div.querySelector("button").addEventListener("click", () => {
 div.querySelector("button").addEventListener("click", () => {
   alert(`"${book.title}" has been added to the cart!`);
 });
+div.querySelector("button").addEventListener("click", () => {
+  alert(`"${book.title}" has been added to the cart!`);
+});div.querySelector("button").addEventListener("click", () => {
+  alert(`"${book.title}" has been added to the cart!`);
+});div.querySelector("button").addEventListener("click", () => {
+  alert(`"${book.title}" has been added to the cart!`);
+});
